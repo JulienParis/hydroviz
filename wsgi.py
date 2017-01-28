aa07d7c0ff4b4ca67022c3ee504df9616abaafc4
Jpy <jparis.py@gmail.com>
2017-01-28 23:32:43 +0100

    WSGI.PY / starting with deployment config

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,12 @@ import os, sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app
+from app import app, socketio
 
 if __name__ == "__main__":
-    app.run()
+    app.run_pesticides()
 
 ### start gunicorn from terminal :
-### from : /apps/portfolio/personal-website
+### from : /apps/concours_pesticides/
 ### run command                 : gunicorn --bind 0.0.0.0:8000 wsgi:app
 ### run command in background   : gunicorn --bind 0.0.0.0:8000 wsgi:app &
