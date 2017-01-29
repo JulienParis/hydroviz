12554c815273aaa62d5c4c14f1565d3d2707f7cc
Jpy <jparis.py@gmail.com>
2017-01-29 07:03:26 +0100

    changed wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,24 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app, socketio
 
-if __name__ == "__main__":
-    app.run_pesticides_prod()
+
+CONFIG = {
+    # 'mode': 'wsgi',
+    'working_dir': '/apps/concours_pesticides',
+    # 'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:5000',
+        '--workers=16',
+        '--timeout=120',
+        'app.run_pesticides_prod',
+    ),
+}
+
+
+# if __name__ == "__main__":
+#     app.run_pesticides_prod()
+
+
 
 ### start gunicorn from terminal :
 ### from : /apps/concours_pesticides/
