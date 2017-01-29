242753ce1bd5b180c1b1d5058a1585fe67f2453b
Jpy <jparis.py@gmail.com>
2017-01-29 07:19:42 +0100

    changed wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,21 +5,21 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app, socketio
 
 
-CONFIG = {
-    # 'mode': 'wsgi',
-    'working_dir': '/apps/concours_pesticides',
-    # 'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:5000',
-        '--workers=3',
-        '--timeout=120',
-        'app.run_pesticides_prod',
-    ),
-}
+# CONFIG = {
+#     # 'mode': 'wsgi',
+#     'working_dir': '/apps/concours_pesticides',
+#     # 'python': '/usr/bin/python',
+#     'args': (
+#         '--bind=0.0.0.0:5000',
+#         '--workers=3',
+#         '--timeout=120',
+#         'app.run_pesticides_prod',
+#     ),
+# }
 
 
-# if __name__ == "__main__":
-#     app.run_pesticides_prod()
+if __name__ == "__main__":
+    app.run_pesticides_prod()
 
 
 
